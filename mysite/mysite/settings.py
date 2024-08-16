fe6b63565ea7bcefe21f8a0b962033f46f682b79
vladimir <vovka1998@gmail.com>
2024-08-16 15:57:37 +0300

    Добавил подгрузку переменных окружения (load_env)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from os import getenv
 from pathlib import Path
 import logging.config
@@ -20,6 +21,7 @@ from django.urls import reverse_lazy
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASE_DIR = BASE_DIR / 'database'
 DATABASE_DIR.mkdir(exist_ok=True)
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -31,9 +33,9 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 DEBUG = getenv('DJANGO_DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "0.0.0.0",
-] + getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+                    "127.0.0.1",
+                    "0.0.0.0",
+                ] + getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -174,25 +176,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = reverse_lazy("myauth:about-me")
 LOGIN_URL = reverse_lazy("myauth:login")
 
-LOGLEVEL = getenv('DJANGO_LOGLEVEL', 'info').upper()
+LOGLEVEL = getenv('DJANGO_LOGLEVEL', default='info').upper()
 logging.config.dictConfig({
     "version": 1,
-        "disable_existing_loggers": True,
-        "formatters": {
-            "console": {
-                "format": f"%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s",
-            }
+    "disable_existing_loggers": True,
+    "formatters": {
+        "console": {
+            "format": f"%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",
         },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
-            },
+    },
+    "loggers": {
+        '': {
+            "level": LOGLEVEL,
+            "handlers": ["console"]
         },
-        "loggers": {
-            '': {
-                "level": LOGLEVEL,
-                "handlers": ["console"]
-            },
-        },
+    },
 })
